22637472905ca74150a7bbd329cb7b1b8b69ed1a
Theologos Mavropoulos <rusinner88@gmail.com>
2022-12-29 12:59:03 +0200

    connected app to reducer

--- crypto-app/src/services/cryptoApi.js
+++ crypto-app/src/services/cryptoApi.js
@@ -4,18 +4,15 @@ const cryptoApiHeaders = {
   "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
 };
 
-const baseUrl = "https";
-const options = {
-  method: "GET",
-  url: "https://coinranking1.p.rapidapi.com/coins",
-  params: {
-    referenceCurrencyUuid: "yhjMzLPhuIDl",
-    timePeriod: "24h",
-    "tiers[0]": "1",
-    orderBy: "marketCap",
-    orderDirection: "desc",
-    limit: "50",
-    offset: "0",
-  },
-  headers: {},
-};
+const baseUrl = "https://coinranking1.p.rapidapi.com/coins";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/exchanges"),
+    }),
+  }),
+});
