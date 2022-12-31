d8868a382cd5198ecfba50820ca3fedf72937882
Theologos Mavropoulos <rusinner88@gmail.com>
2022-12-31 15:54:15 +0200

    cryptoDetails 24h volume

--- crypto-app/src/components/Cryptocurrencies.jsx
+++ crypto-app/src/components/Cryptocurrencies.jsx
@@ -31,10 +31,16 @@ const Cryptocurrencies = ({ simplified }) => {
 
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency) => (
-          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
-            <Link to={`/crypto/${currency.id}`}>
+          <Col
+            xs={24}
+            sm={12}
+            lg={6}
+            className="crypto-card"
+            key={currency.uuid}
+          >
+            <Link to={`/crypto/${currency.uuid}`}>
               <Card
-                key={currency.id}
+                key={currency.uuid}
                 title={`${currency.rank}. ${currency.name}`}
                 extra={
                   <img
